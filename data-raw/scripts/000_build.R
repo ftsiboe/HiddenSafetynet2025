56e5d4ddb14341a5e7ebec71233eb8550691fedc
Francis Tsiboe <69377090+ftsiboe@users.noreply.github.com>
2025-09-06 01:39:17 -0500

    updates

--- data-raw/scripts/000_build.R
+++ data-raw/scripts/000_build.R
@@ -1,20 +1,25 @@
 
-rm(list=ls(all=TRUE));gc();library(rfcip);library(data.table)
-unlink(c("NAMESPACE","./R/helper_data.R",
-         list.files("./data", full.names = TRUE),
-         list.files("./man", full.names = TRUE)))
-rm(list=ls(all=TRUE))
+rm(list=ls(all=TRUE));gc()
+
+library(data.table);library(rfcip);library(rfcipCalcPass)
+
+unlink(c("NAMESPACE",list.files("./data", full.names = TRUE),list.files("./man", full.names = TRUE)))
+
 devtools::document()
+
 for(i in list.files("R",full.names = T)){
   print(paste0("********************",i,"********************"))
   tools::showNonASCIIfile(i)
 }
+
 devtools::check_man()
+
 devtools::build_manual(path = getwd())
-devtools::test()
 
-devtools::check()
+#devtools::test()
 
-devtools::document()
+#devtools::check()
+
 study_env <- setup_environment()
+
 clean_rma_sobtpu()
